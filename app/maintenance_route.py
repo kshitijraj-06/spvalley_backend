7c04929ba0cf68f66aec1c6ae61d3fd59033b849
kshit <kshitij6102004@gmail.com>
2025-03-17 22:09:43 +0530

    Added maintenance_route.py

--- app/maintenance_route.py
+++ app/maintenance_route.py
@@ -2,3 +2,13 @@ from flask import Flask, request, jsonify, Blueprint
 from .database import get_db_connection
 
 maintenance_route = Blueprint('maintenance', __name__)
+
+@maintenance_route.route('/maintenance', methods=['GET'])
+def maintenance():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM maintenance")
+        maintenance_data = cur.fetchall()
+        cur.close()
+
